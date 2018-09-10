e1c7cd412737f5369736add216f5e58c0b49f6f7
wenzhenglin <wenzhenglin@qianbao.com>
2018-09-10 18:34:52 +0800

    update verify-upstream-content to adapt pre-env

--- verify-upstream-content.sh
+++ verify-upstream-content.sh
@@ -3,9 +3,10 @@
 # kubectl delete pod `kubectl get pods -n qb-qa-10 | grep ops-fs | awk '{ print $1 }' |grep -v NAME | head -1 ` -n  qb-qa-10
 # kubectl scale deploy ops-fs -n qb-qa-10 --replicas=2; 
 
-file=/apps/soft/nginx/conf.d/upstream/ops_fs_upstream_qb-qa-10.conf
+file="${1:=/apps/soft/nginx/conf.d/upstream/ops_fs_upstream_qb-qa-10.conf}"
+echo "usng $file as nginx upstream file"
 while true; do 
-  out="$( cat $file | grep server | awk '{ print $2 }' )"
+  out="$( cat $file | grep -v '^#' | grep server | awk '{ print $2 }' )"
   if [ "x$out" != "x$prev" ]; then
     extra="changed"
   else
